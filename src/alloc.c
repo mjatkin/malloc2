32857c1a48339537517af5d6ecc7ec8f4604b50a
mjatkin <matthewjamesatkin@gmail.com>
2019-08-25 15:41:27 +1000

    Baic alloc and dealloc

--- src/alloc.c
+++ src/alloc.c
@@ -6,6 +6,7 @@
  * 
  * Aug 2019
  */
+#include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -22,45 +23,107 @@ static struct block* freed_list_tail = NULL;
 static void* change_break(size_t chunk_size)
 {
     #ifdef DEBUG
-    printf("-->Moving program break %ld bytes forward. (%p -> %p)\n", chunk_size, sbrk(0), ((char*) sbrk(0)) + chunk_size);
+    printf("-->Moving program break %ld bytes forward. (%p -> %p)\n", 
+            chunk_size, sbrk(0), ((char*) sbrk(0)) + chunk_size);
     #endif
     return sbrk(chunk_size);
 }
 
-static struct block* create_block()
+static struct block* create_block(size_t chunk_size)
 {
     #ifdef DEBUG
     printf("-->Creating new block...\n");
     #endif
-    return (struct block*) change_break(sizeof(struct block));
+
+    struct block* current_block = 
+        (struct block*) change_break(sizeof(struct block));
+    
+    current_block->next = NULL;
+    current_block->prev = NULL;
+    current_block->size = chunk_size;
+    current_block->data = change_break(chunk_size);
+
+    #ifdef DEBUG
+    printf("-->Created block (Next: %p, Prev: %p, Size: %ld, Data: %p)\n", 
+            (void*) current_block->next, (void*) current_block->prev,
+            current_block->size, current_block->data);
+    #endif
+    return current_block;
 }
 
-static void freed_list_init()
+static void alloc_list_append(struct block* block)
 {
     #ifdef DEBUG
-    printf("-->Initialising freed list...\n");
+    printf("-->Appending block (Next: %p, Prev: %p, Size: %ld, Data: %p) to"
+            " back of alloc_list.\n", 
+            (void*) block->next, (void*) block->prev, block->size, block->data);
     #endif
-    freed_list_head = create_block();
-    freed_list_tail = freed_list_head;
+
+    if(alloc_list_head == NULL)
+    {
+        alloc_list_head = block;
+    }
+    if(alloc_list_tail != NULL)
+    {
+        alloc_list_tail->next = block;
+        block->prev = alloc_list_tail;
+    }
+    alloc_list_tail = block;
 }
 
-static void alloc_list_init()
+static void freed_list_append(struct block* block)
 {
     #ifdef DEBUG
-    printf("-->Initialising alloc list...\n");
+    printf("-->Appending block (Next: %p, Prev: %p, Size: %ld, Data: %p) to"
+            " back of freed_list.\n", 
+            (void*) block->next, (void*) block->prev, block->size, block->data);
     #endif
-    alloc_list_head = create_block();
-    alloc_list_tail = alloc_list_head;
+    if(freed_list_head == NULL)
+    {
+        freed_list_head = block;
+    }
+    if(freed_list_tail != NULL)
+    {
+        freed_list_tail->next = block;
+        block->prev = freed_list_tail;
+    }
+    freed_list_tail = block;
 }
 
-static void alloc_list_add_back()
+static void list_delete(struct block* block)
 {
+    #ifdef DEBUG
+    printf("-->Removing block (Next: %p, Prev: %p, Size: %ld, Data: %p) to\n", 
+            (void*) block->next, (void*) block->prev, block->size, block->data);
+    #endif
+    if(block == alloc_list_head)
+    {
+        alloc_list_head = block->next;
+    } 
+    else if(block == freed_list_head)
+    {
+        freed_list_head = block->next;
+    }
 
-}
+    if(block == alloc_list_tail)
+    {
+        alloc_list_tail = block->prev;
+    } 
+    else if(block == freed_list_tail)
+    {
+        freed_list_tail = block->prev;
+    }
 
-static void freed_list_add_back()
-{
-
+    if(block->next != NULL)
+    {
+        block->next->prev = block->prev;
+        block->next = NULL;
+    }
+    if(block->prev != NULL)
+    {
+        block->prev->next = block->next;
+        block->prev = NULL;
+    }
 }
 
 static void* alloc_first(size_t chunk_size)
@@ -71,9 +134,8 @@ static void* alloc_first(size_t chunk_size)
     {
         if(current_block->size >= chunk_size)
         {   
-            /* HERE! */
-            alloc_list_add_back(current_block);
             list_delete(current_block);
+            alloc_list_append(current_block);
             return current_block->data;
         }
         current_block = current_block->next;
@@ -82,8 +144,7 @@ static void* alloc_first(size_t chunk_size)
     #ifdef DEBUG
     printf("-->No valid block found...");
     #endif
-
-    alloc_list_add_back();
+    return (create_block(chunk_size)->data);
 }
 
 static void* alloc_best(size_t chunk_size)
@@ -111,12 +172,8 @@ void* alloc(size_t chunk_size)
         #ifdef DEBUG
         printf("-->Freed list empty...\n");
         #endif
-        alloc_list_init();
-        alloc_list_head->data = change_break(chunk_size);
-        alloc_list_head->size = chunk_size;
-        alloc_list_head->next = NULL;
-        alloc_list_head->prev = NULL;
-        return alloc_list_head->data;
+        alloc_list_append(create_block(chunk_size));
+        return alloc_list_tail->data;
     }
 
     switch(current_stratergy)
@@ -146,7 +203,25 @@ void* alloc(size_t chunk_size)
 
 void dealloc(void* chunk)
 {
+    struct block* current_block = alloc_list_head;
     
+    do
+    {
+            printf("Current1: %p\n", current_block->data); 
+            printf("Current2: %p\n", chunk); 
+        if(current_block->data == chunk)
+        {  
+            list_delete(current_block);
+            freed_list_append(current_block);
+            return;
+        }
+        current_block = current_block->next;
+    } while(current_block->next != NULL);
+
+    #ifdef DEBUG
+    printf("-->No valid block found...");
+    #endif
+    exit(3);
 }
 
 void set_stratergy(enum stratergy stratergy)
--- src/main.c
+++ src/main.c
@@ -7,7 +7,22 @@ int main(int argc, char* argv[])
         printf("-->DEBUG ENABLED\n");
     #endif
 
-    alloc(3);
+    void* foo[10];
+
+    for(int i = 0; i < 10; ++i)
+    {
+        foo[i] = alloc(i);
+    }
+
+    for(int i = 0; i < 10; ++i)
+    {
+        printf("Main: %p\n", foo[i]);
+    }
+
+    for(int i = 1; i < 10; ++i)
+    {
+        dealloc(foo[i]);
+    }
 
     printf("Main terminated.\n");
     return 0;
